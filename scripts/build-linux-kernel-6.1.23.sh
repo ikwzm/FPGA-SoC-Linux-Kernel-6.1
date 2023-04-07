739cbb45a705ac1e9f3f23fa8cfb4197b7c0c875
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2023-04-07 12:04:01 +0900

    [add] scripts and documents for build linux-kernel-6.1.23-armv7-fpga

---
+++ scripts/build-linux-kernel-6.1.23.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+CURRENT_DIR=`pwd`
+KERNEL_VERSION=6.1.23
+LOCAL_VERSION=armv7-fpga
+BUILD_VERSION=1
+KERNEL_RELEASE=$KERNEL_VERSION-$LOCAL_VERSION
+LINUX_BUILD_DIR=linux-$KERNEL_RELEASE
+
+echo "KERNEL_RELEASE  =" $KERNEL_RELEASE
+echo "BUILD_VERSION   =" $BUILD_VERSION
+echo "LINUX_BUILD_DIR =" $LINUX_BUILD_DIR
+
+## Download Linux Kernel Source
+
+### Clone from linux-stable.git
+
+git clone --depth 1 -b v$KERNEL_VERSION git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git $LINUX_BUILD_DIR
+
+### Make Build Branch
+
+cd $LINUX_BUILD_DIR
+git checkout -b $KERNEL_RELEASE refs/tags/v$KERNEL_VERSION
+
+## Patch to Linux Kernel
+
+### Patch for armv7-fpga
+patch -p1 < ../patchs/linux-$KERNEL_VERSION-armv7-fpga.diff
+git add --update
+git add arch/arm/configs/armv7_fpga_defconfig
+git add arch/arm/boot/dts/zynq-pynqz1.dts
+git commit -m "patch for armv7-fpga"
+
+### Patch for usb chipidea driver
+patch -p1 < ../patchs/linux-$KERNEL_VERSION-armv7-fpga-patch-usb-chipidea.diff
+git add --update
+git commit -m "patch for usb chipidea driver for issue #3"
+
+### Patch for build debian package script
+patch -p1 < ../patchs/linux-$KERNEL_VERSION-armv7-fpga-patch-builddeb.diff
+git add --update
+git commit -m "patch for scripts/package/builddeb to add tools/include and arch/arm/tools and postinst script to header package"
+
+### Create tag and .version
+git tag -a $KERNEL_RELEASE-$BUILD_VERSION -m "release $KERNEL_RELEASE-$BUILD_VERSION"
+echo `expr $BUILD_VERSION - 1` > .version
+
+## Build
+
+### Setup for Build 
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+make armv7_fpga_defconfig
+
+### Build Linux Kernel and device tree
+export DTC_FLAGS=--symbols
+make deb-pkg
+make zynq-zybo.dtb
+make zynq-zybo-z7.dtb
+make zynq-pynqz1.dtb
+make socfpga_cyclone5_de0_nano_soc.dtb
+ 
+## Install
+
+### Install kernel image to this repository
+
+cp arch/arm/boot/zImage ../vmlinuz-$KERNEL_RELEASE-$BUILD_VERSION
+cp .config              ../files/config-$KERNEL_RELEASE-$BUILD_VERSION
+
+### Install devicetree to this repository
+
+install -d ../devicetrees/$KERNEL_RELEASE-$BUILD_VERSION
+cp arch/arm/boot/dts/zynq*    ../devicetrees/$KERNEL_RELEASE-$BUILD_VERSION
+cp arch/arm/boot/dts/socfpga* ../devicetrees/$KERNEL_RELEASE-$BUILD_VERSION
+
