33bc95b272bc9e8592e5d50ad2f042336a718ab2
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2023-07-29 05:47:33 +0900

    [add] scripts for install linux-kernel-6.1.42-armv7-fpga

---
+++ scripts/install-linux-6.1.42-armv7-fpga.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+REPO_DIR=$(cd $(dirname $0); cd .. ; pwd)
+KERNEL_VERSION=6.1.42
+LOCAL_VERSION=armv7-fpga
+BUILD_VERSION=1
+
+. "$SCRIPT_DIR/install-variables-armv7-fpga.sh"
+. "$SCRIPT_DIR/install-command.sh"
